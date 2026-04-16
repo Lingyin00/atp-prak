519307d686af681b1e54f51af29aa769b85897c8
Lingyin Luo <Luo.Lingyin@campus.lmu.de>
2026-04-16 23:31:49 +0200

    exercise 1.1

--- exercise/playground/Playground/Term.lean
+++ exercise/playground/Playground/Term.lean
@@ -19,3 +19,26 @@ def FV : Term → List String
 
 /-A data structure for substitutions. A substitution is a finite map from the free vari-
 ables of a term to terms-/
+def Substitution := String → Option Term
+
+/-The example of a substitution in the exercise sheet-/
+def exampleSub : Substitution
+  | "x" => some (Term.func "f" [Term.func "a" [] , Term.func "b" []])
+  | "y" => some (Term.func "c" [])
+  | "z" => some (Term.var "y")
+  | _ => none
+
+/-An operation subst, that takes a term and a substitution and applies the substitution
+to the term-/
+def subst : Term → Substitution → Term
+  | Term.var a, sub =>
+    match (sub a) with
+      | none => Term.var a
+      | some arg => arg
+  | Term.func f xs, sub => Term.func f (substList xs sub)
+    where
+      substList : List Term → Substitution → List Term
+        | [], _ => []
+        | x :: xs, sub => subst x sub :: (substList xs sub)
+
+#eval subst (Term.func "f" [Term.var "x", Term.var "y"]) exampleSub
